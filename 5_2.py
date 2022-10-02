7c9354d9dbd3538a9de71ff80e154bd0bb779c8a
Anna <podobedanna88008@gmail.com>
2022-10-02 17:06:55 +0300

    task 5_2

--- 5_2.py
+++ 5_2.py
@@ -1,9 +1,9 @@
 # Сделать калькулятор: у пользователя
 # спрашивается число, потом действие и второе
 # число
-numb1 = int(input())
+numb1 = float(input())
 action = input()
-numb2 = int(input())
+numb2 = float(input())
 if action == '+':
     print(numb1 + numb2)
 elif action == '-':
