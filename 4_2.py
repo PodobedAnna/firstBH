120b5104a5512f8d85bfbb0a2ec8b01ff5af9aa6
Anna <podobedanna88008@gmail.com>
2022-10-02 15:55:18 +0300

    task 4_2

--- 4_2.py
+++ 4_2.py
@@ -2,4 +2,9 @@
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
 
-text=input()
+text=input('введите текст: ')
+text2 = set(text)
+text_dict = dict.fromkeys(text2, 0)
+for text2 in text:
+    text_dict [text2] += 1
+print(text_dict)
